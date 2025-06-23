ea8e93a307fdf3517ad1f1957edf130b7594b987
Konrad Kreczko <konradkreczko@gmail.com>
2025-06-23 21:42:30 +0200

    Basic :rofl:

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+
 int main()
 {
-  return 0;
+  int imageWidth = 256;
+  int imageHeight = 256;
+
+  std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+  for (int j = 0; j < imageHeight; j++)
+  {
+    for (int i = 0; i < imageWidth; i++)
+    {
+      double r = double(j) / (imageWidth - 1);
+      double g = double(i) / (imageHeight - 1);
+      double b = 0.0;
+
+      int ir = int(255.999 * r);
+      int ig = int(255.999 * g);
+      int ib = int(255.999 * b);
+
+      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+    }
+  }
 }
