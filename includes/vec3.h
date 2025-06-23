9c7ba783c525c1632611aedf7b6b111d943a623e
Konrad Kreczko <konradkreczko@gmail.com>
2025-06-23 22:56:55 +0200

    Started 3D vector

---
+++ includes/vec3.h
@@ -0,0 +1,74 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+class Vector_3D
+{
+  public:
+    double m_Cords[3];
+
+    Vector_3D() : m_Cords{0, 0, 0} {}
+    Vector_3D(double x, double y, double z)
+      : m_Cords{x, y, z} {}
+
+    double getX() const { return m_Cords[0]; }
+    double getY() const { return m_Cords[1]; }
+    double getZ() const { return m_Cords[2]; }
+
+    Vector_3D operator-() const { return Vector_3D(-m_Cords[0], -m_Cords[1], -m_Cords[2]); }
+    double operator[](int i) const { return m_Cords[i]; }
+    double& operator[](int i) { return m_Cords[i]; }
+
+    Vector_3D& operator+=(const Vector_3D& other)
+    {
+      m_Cords[0] += other.m_Cords[0];
+      m_Cords[1] += other.m_Cords[1];
+      m_Cords[2] += other.m_Cords[2];
+      return *this;
+    }
+
+    Vector_3D& operator*=(double t)
+    {
+      m_Cords[0] *= t;
+      m_Cords[1] *= t;
+      m_Cords[2] *= t;
+      return *this;
+    }
+
+    Vector_3D& operator/=(double t)
+    {
+      return *this *= 1/t;
+    }
+
+    double length() const
+    {
+      return std::sqrt(lengthSquared());
+    }
+
+    double lengthSquared() const
+    {
+      return m_Cords[0] * m_Cords[0] + m_Cords[1] * m_Cords[1] + m_Cords[2] * m_Cords[2];
+    }
+};
+
+using Point_3D = Vector_3D;
+
+inline std::ostream& operator<<(std::ostream& out, const Vector_3D& v)
+{
+  return out << v.m_Cords[0] << ' ' << v.m_Cords[1] << ' ' << v.m_Cords[2];
+}
+
+inline Vector_3D operator+(const Vector_3D& u, const Vector_3D& v)
+{
+  return Vector_3D(
+      u.m_Cords[0] + v.m_Cords[0],
+      u.m_Cords[1] + v.m_Cords[1],
+      u.m_Cords[2] + v.m_Cords[2]
+  );
+}
+
+// TODO v-u, v*u, t*v, v*t, v/t, dot, cross, unit 
+
+#endif // !VEC3_H
