e7d808d3e2b1dd2523f9be26318fb5fef887cf99
Konrad Kreczko <konradkreczko@gmail.com>
2025-06-24 22:00:02 +0200

    Added write color func :rofl:

--- includes/color.h
+++ includes/color.h
@@ -6,6 +6,17 @@
 
 using color = Vector_3D;
 
+void writeColor(std::ostream& out, const color& pixelColor)
+{
+  double r = pixelColor.getX();
+  double g = pixelColor.getY();
+  double b = pixelColor.getZ();
 
+  int rbyte = int(255.999 * r);
+  int gbyte = int(255.999 * g);
+  int bbyte = int(255.999 * b);
+
+  out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
 
 #endif // !COLOR_H
