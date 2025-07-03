f8fc6665f9b24e3f2eb5646a2cbfbe6d19e5301f
Konrad Kreczko <konradkreczko@gmail.com>
2025-07-03 22:51:42 +0200

    Added height and width calculations :rofl:

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,14 @@
 
 int main()
 {
-  int imageWidth = 256;
-  int imageHeight = 256;
+  double aspect_ratio = 16.0 / 9.0;
+  int image_width = 400;
 
-  std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+  int image_heigth = int(image_width / aspect_ratio);
+  image_heigth = (image_heigth < 1) ? 1 : image_heigth;
 
-  for (int j = 0; j < imageHeight; j++)
-  {
-    std::clog << "\rScanlines remaining: " << (imageHeight - j) << ' ' << std::flush;
-    for (int i = 0; i < imageWidth; i++)
-    {
-      auto pixelColor = Color(
-        double(j) / (imageWidth - 1),
-        double(j) / (imageHeight - 1),
-        0
-      );
+  double viewport_height = 2.0;
+  double viewport_width = viewport_height * (double(image_width) / image_heigth);
 
-      writeColor(std::cout, pixelColor);
-    }
-  }
-
-  std::clog << "\rDone\n";
+  return 0;
 }
